b19f356774c1e5f0cbf1e149415f821c55c005fc
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-25 11:29:52 -0300

    Find all cities

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> result = repository.findAll(Sort.by("name"));
+        return result.stream().map(city -> new CityDTO(city.getId(), city.getName())).collect(Collectors.toList());
+    }
+
+}
